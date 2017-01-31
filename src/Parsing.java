52b0d412e17b1045882e823065c2d616886daacc
CHOIMINSEOK <cms3718@gmail.com>
2017-01-31 11:16:14 +0900

    몇가지 예외처리 - 카톡 실행중인지 체크해서 카톡 실행

--- src/Parsing.java
+++ src/Parsing.java
@@ -9,7 +9,6 @@ import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.*;
 import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.win32.W32APIOptions;
-import com.sun.jna.platform.win32.WinDef.*;
 
 public class Parsing {
 	final static int WM_SETTEXT = 0x000C;
@@ -17,49 +16,59 @@ public class Parsing {
 	
 	public static void main(String[] args)throws Exception{
 
-		//Document doc = Jsoup.connect("http://www.aladin.co.kr/shop/common/wbest.aspx?BranchType=1&start=we_tab").get();
-		//Elements titles = doc.select("a[href*=javascript]");
-		//Elements titles = doc.select(".bo3");
-		//Elements authors = doc.select("a[href*=/Search/wSearchResult.aspx?AuthorSearch]");
-		/*
-		for(Element e: titles){
+		Document doc = Jsoup.connect("http://electronic.hanyang.ac.kr/notice/list_hi.php").get();
+		Elements notices = doc.select("a[href*=javascript]");
 
-			System.out.println( e.text());
-		}
-		for(Element e: authors){
-
-			System.out.println( e.text());
-		}
-*/	
+		//카톡 켜져있는지 확인
+		HWND hKakao = User32.INSTANCE.FindWindow(null, "카카오톡");
+		HWND hChat;
 		
-		HWND hwnd = User32.INSTANCE.FindWindow(null, "밈석"); // window title
-		HWND h;
-		if (hwnd == null) {
-			System.out.println("Excel is not running");
+		if(hKakao == null){
+			//카카오톡 실행
+			Runtime.getRuntime().exec("C:\\Program Files (x86)\\Kakao\\KakaoTalk\\KakaoTalk.exe");
 		}
-		else{
-			User32.INSTANCE.ShowWindow(hwnd, 9 );        // SW_RESTORE
-			User32.INSTANCE.SetForegroundWindow(hwnd);   // bring to front
+		//User32.INSTANCE.SetWindowPos(hKakao,  , 0, 0, 0, 0, 0);
+		
+		
+		
+		
+		//채팅방 확인
+		hKakao = User32.INSTANCE.FindWindow(null, "지음"); // window title
+		
+		if (hKakao == null) {
+			System.out.println("카톡을 키세요");
+			return ;
+		}
+		
+		hChat = User32.INSTANCE.FindWindowEx(hKakao, null, "RichEdit20W", null);
+		User32.INSTANCE.ShowWindow(hKakao, 9 );        // SW_RESTORE
+		User32.INSTANCE.SetForegroundWindow(hKakao);   // bring to front
+		
+		for(Element e: notices){
+			SendText(hChat, e.text());
+			System.out.println( e.text());
+		}
+
 			
-			h = User32.INSTANCE.FindWindowEx(hwnd, null, "RichEdit20W", null);
-			SendText(h);
-		}
-	
-		
-
-		
-
 	}
 	
-	public static void SendText(HWND hEdit) {		
-	    MyUser32.INSTANCE.SendMessage(hEdit, WM_SETTEXT, 0, "test\0".toCharArray());
+	public static void SendText(HWND hEdit, String notice) {		
+	    MyUser32.INSTANCE.SendMessage(hEdit, WM_SETTEXT, 0, notice);
+	   
+	    try{
+	    	Thread.sleep(500);
+	    } catch(InterruptedException e){
+	    	e.getMessage();
+	    }
+	    
 	    MyUser32.INSTANCE.PostMessage(hEdit, User32.WM_KEYDOWN, VK_ENTER, 0);
 	    MyUser32.INSTANCE.PostMessage(hEdit, User32.WM_KEYUP, VK_ENTER, 0);
 	}
 	public interface MyUser32 extends User32 {
 	    MyUser32 INSTANCE = (MyUser32)Native.loadLibrary("user32", MyUser32.class, W32APIOptions.DEFAULT_OPTIONS);
-	    LRESULT SendMessage(HWND hWnd, int Msg, int wParam, char[] lParam);
+	    LRESULT SendMessage(HWND hWnd, int Msg, int wParam, String lParam);
 	    int PostMessage(HWND hWnd, int msg, int wParam, int lParam); 
+	    
 	}
 
 }
